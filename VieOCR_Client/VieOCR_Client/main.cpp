2fa55edb51e53705affd528aa11ae0055ec2b88b
hakuo <dhc1995@gmail.com>
2018-01-09 15:33:29 +0700

    add clean up function

--- VieOCR_Client/VieOCR_Client/main.cpp
+++ VieOCR_Client/VieOCR_Client/main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 int main()
 {
     //setenv("TOOL_SYS_ROOT", "/home/cuongdh8/workspace/qt/cpp/mythesis17/VieOCR_Client", 1);
-    setenv("TOOL_SYS_ROOT", "/home/pi/workspace/mythesis17/VieOCR_Client", 1);
+    std::string system_root = "/home/pi/workspace/mythesis17/VieOCR_Client";
+    setenv("TOOL_SYS_ROOT", system_root.c_str(), 1);
     Task *pTask = new Task();
     pTask->initParameter(TCP_PORT, 1, 1, "192.168.1.38");
     pTask->runAllTask();
-    cout << "Client is running... Enter 'c' to capture or 'e' to exit" << endl;
-
     // Block program here
     char input;
     while (true)
     {
+        cout << "Client is running... Enter 'c' to capture or 'a' to clean up tmp_dir or 'e' to exit" << endl;
         input = getchar();
         if('e' == input)
         {
@@ -25,6 +25,13 @@ int main()
         if('c' == input)
         {
             pTask->capture();
+        }
+
+        if('a' == input)
+        {
+            std::string cmd;
+            cmd = "rm -rf " + system_root + TMP_PATH;
+            system(cmd.c_str());
         }
     }
 
--- VieOCR_Server/VieOCR_Server/main.cpp
+++ VieOCR_Server/VieOCR_Server/main.cpp
@@ -5,17 +5,27 @@ using namespace std;
 
 int main()
 {
-    setenv("TOOL_SYS_ROOT", "/home/cuongdh8/workspace/qt/cpp/mythesis17/VieOCR_Server", 1);
+    std::string system_root = "/home/cuongdh8/workspace/qt/cpp/mythesis17/VieOCR_Server";
+    setenv("TOOL_SYS_ROOT", system_root.c_str(), 1);
     Task *pTask = new Task(OCR::TESSERACT_OCR);
     pTask->initParameter(TCP_PORT, 1);
     pTask->runAllTask();
 
-    cout << "Server is running... Enter 'e' to exit" << endl;
+    //cout << "Server is running... Enter 'e' to exit" << endl;
     //Block program here
+    char input;
     while (true) {
-        if('e' == getchar())
+        cout << "Server is running... Enter 'e' to exit or 'a' to clean up tmp_dir" << endl;
+        input = getchar();
+        if('e' == input)
         {
             break;
+        }
+        if('a' == input)
+        {
+            std::string cmd;
+            cmd = "rm -rf " + system_root + TMP_PATH;
+            system(cmd.c_str());
         }
     }
     pTask->stopAllTask();
